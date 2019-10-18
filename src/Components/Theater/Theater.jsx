76df1cf8f4965cac89ba32726fa79c411887a1b2
Michal Gazdik <gazdikm@seznam.cz>
2019-10-18 14:01:24 +0200

    json

--- src/Components/Theater/Theater.jsx
+++ src/Components/Theater/Theater.jsx
@@ -4,31 +4,87 @@ import NowPlaying from '../NowPlaying';
 import PlayingToday from '../PlayingToday';
 import UpcomingMovies from '../UpcomingMovies';
 
-export default function Theater(props) {
-    return (
+export default class Theater extends React.Component {
+    
+        constructor(props) {
+            super(props);
+    
+            this.state = {
+                loading: false,
+                loaded: false,
+                data: []
+            };
+    
+            this.url = "http://www.cinemalist.test:8080/api/tableTheatre";
+        }
+        componentDidMount() {
+            this.loadData();    
+        }
+    
+        loadData() {
+            if (this.url) {
+                this.setState({ 
+                    loading: true,
+                    loaded: false,
+                    data: []
+                })
+    
+                fetch(this.url)
+                    .then(response => response.json())
+                    .then(data => {
+                        this.setState({ 
+                            loaded: true,
+                            data: data
+                        })
+                    })
+                    .finally(this.setState({
+                        loading: false
+                    }));
+            }
+        }
+
+        render () {
+            let content = (
+                <div className="theatre">
+                   
+                </div>
+            )
+        if (!this.state.loading && this.state.loaded) {
+            content = (
+        
+       
         <>
             <Header 
                 slug={ props.slug }
-            />
+                />
 
-            <h1>{ props.data.name }</h1>
+                <h1>{ props.data.name }</h1>
 
-            <div className="page__columns">
+                <div className="page__columns">
 
-                <main>
-                    <PlayingToday />
+                    <main>
+                        <PlayingToday />
 
-                    <UpcomingMovies />
-                </main>
+                        <UpcomingMovies />
+                    </main>
 
-                <aside>
+                    <aside>
 
-                    <NowPlaying />
-                    
-                </aside>
+                        <NowPlaying />
+                        
+                    </aside>
 
-            </div>
-        </>
+                </div>
+            </>
         
-    )
+            )
+        }
+        return (
+            <section className="theaters">
+
+            { content }
+
+            </section>
+        );
+    }
 }
